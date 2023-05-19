bd6d93d96d773b0df692e2d77f1f612187bfbcf3
Iskender Shamaldaev <sh.iskander012@gmail.com>
2023-05-19 12:07:20 +0600

    Added pictures and made a CardView component.

---
+++ src/СardView/СardView.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+interface IPersonProps {
+    rank: string;
+    suit: string;
+}
+
+const CardView: React.FC<IPersonProps> = props => {
+    const cardClasses = `card rank-${props.rank} ${props.suit}`;
+
+    return (
+        <div className="playingCards faceImages">
+            <span className={cardClasses}>
+              <span className="rank">{props.rank}</span>
+              <span className="suit">{props.suit}</span>
+            </span>
+        </div>
+    );
+};
+
+export default CardView;

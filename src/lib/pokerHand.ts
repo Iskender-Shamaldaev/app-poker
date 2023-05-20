3e9d1a32cd39c57e024eae7db43b755930937d0c
Iskender Shamaldaev <sh.iskander012@gmail.com>
2023-05-21 02:40:36 +0600

    Defined combination 'Покер' and refactor.

--- src/lib/pokerHand.ts
+++ src/lib/pokerHand.ts
@@ -7,40 +7,41 @@ class PokerHand {
 
 
     getOutcome() {
-        const cards = this.cards;
+
         let pairCount = 0;
         let flush = true;
 
         for (let i = 0; i < 4; i++) {
-            const rankI = cards[i].rank;
-            const suitI = cards[i].suit;
+            const rankI = this.cards[i].rank;
+            const suitI = this.cards[i].suit;
             for (let j = i + 1; j < 5; j++) {
-                const rankJ = cards[j].rank;
-                const suitJ = cards[j].suit;
+                const rankJ = this.cards[j].rank;
+                const suitJ = this.cards[j].suit;
                 if (rankI === rankJ) {
                     pairCount++;
                 }
                 if (suitI !== suitJ) {
                     flush = false;
-                    break;
                 }
             }
         }
 
-        if (flush) {
-            return 'Флэш';
-        } else if (pairCount === 1) {
-            return 'Одна пара';
+        if (pairCount === 1) {
+            return('Одна пара');
         } else if (pairCount === 2) {
-            return 'Две пары';
+            return('Две пары');
         } else if (pairCount === 3) {
-            return 'Тройка';
+            return('Тройка');
+        } else if (pairCount === 4) {
+            return ('Покер');
+        }else if (flush) {
+            return('Флэш');
         } else {
-            return 'Нет комбинации';
+            return('Нет комбинации');
         }
     };
 
 
 }
 
-export default PokerHand;
+    export default PokerHand;
